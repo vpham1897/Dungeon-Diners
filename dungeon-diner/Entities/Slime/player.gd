877b6ac257038bd043f14a8d8168f421668e7ea9
myshyfun <atanasd2004@gmail.com>
2025-07-10 16:08:37 -0600

    very dashing
    dashing is still weak af for some reason

--- dungeon-diner/Entities/Slime/player.gd
+++ dungeon-diner/Entities/Slime/player.gd
@@ -2,19 +2,24 @@ extends CharacterBody2D
 
 signal use_item(source : Node2D)
 
-var maxSPEED = 800
-var Acceleration = 300
-var Deceleration = 600
-var target = Vector2.ZERO
-var cooldown = 0
-# what attack cooldown resets.
-var maxCooldown = 1
+@export var maxSPEED = 10000
+var Acceleration
+var Deceleration 
+
+@export var dash_strength = 100000000000000000
+var dash_cooldown = 0
+var max_dash_cooldown = 10
+var dash_duration: float = .5
+var dash_timer: float = 0.0
+var is_dashing: bool = false
+var dashing_direction: Vector2 = Vector2.ZERO
 
 #tileset variables
 @export var data_tile_map: TileMapLayer
-
 var currentTitleData
 var tile_data
+@export var High = 400
+@export var Low = 200
 func _ready():
 	if data_tile_map!= null: #does FloorData exist
 		tile_data = data_tile_map.get_tile_info_at(global_position)
@@ -45,8 +50,6 @@ func _physics_process(delta):
 
 
 #Balance speed values
-	var High = 500
-	var Low = 100
 	if currentTitleData == "Crisp":
 		Acceleration = High
 		Deceleration = High
@@ -56,34 +59,62 @@ func _physics_process(delta):
 	if currentTitleData == "Sticky" :
 		Acceleration = Low
 		Deceleration = High
+#dashing
 
-
-#Input movement commands
 	var direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	
-	if direction != Vector2.ZERO:
-		velocity = velocity.move_toward(direction * maxSPEED, Acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, Deceleration * delta)
+	if Input.is_action_just_pressed("dash") and not is_dashing and dash_cooldown == 0:
+		var dashdirection = (get_global_mouse_position()-global_position).normalized()
+		
+		velocity = dashdirection * dash_strength
+		dash_timer = dash_duration
+		is_dashing = true
+		print("dashing")
+	
+	# If dashing, reduce timer but still allow normal input smoothing
+	if is_dashing:
+		dash_timer -= delta
+		print("slowing down dashing")
+		if dash_timer <= 0:
+			is_dashing = false
+			print("done dashing")
+
+	# Normal smooth movement only when not dashing
+	if not is_dashing:
+		if direction != Vector2.ZERO:
+			velocity = velocity.move_toward(direction * maxSPEED, Acceleration * delta)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, Deceleration * delta)
+
 	move_and_slide()
+
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
 #endregion
 
-	if cooldown <= 0:
+	if item_cooldown <= 0:
 		look_at(get_global_mouse_position())
 	else:
-		cooldown -= delta
-	
+		item_cooldown -= delta
+	if dash_cooldown <= 0:
+		look_at(get_global_mouse_position())
+	else:
+		dash_cooldown -= delta
+
+var target = Vector2.ZERO
+var item_cooldown = 0
+var max_item_cooldown = 1
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if cooldown <= 0:
+		if item_cooldown <= 0:
 			print("using item!")
 			InventoryManager.use_item(self)
-			cooldown = maxCooldown
+			item_cooldown = max_item_cooldown
 		else:
-			print("on cooldown! ", cooldown)
+			print("item on cooldown! ", item_cooldown)
 		# Call inventory manager's instantiate and give my stuffs
+
 		
