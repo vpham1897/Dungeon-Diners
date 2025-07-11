363a6295005cf5e251120aa64381bae76d05ae35
vpham1897 <94569780+vpham1897@users.noreply.github.com>
2025-07-11 10:59:37 -0600

    Reorganization and dash fixed
    Dashes are acceleration based. Speed is capped

--- dungeon-diner/Entities/Slime/player.gd
+++ dungeon-diner/Entities/Slime/player.gd
@@ -2,21 +2,24 @@ extends CharacterBody2D
 
 signal use_item(source : Node2D)
 
-var maxSPEED = 800
-var BaseAcceleration = 300
-var BaseDeceleration = 600
-var target = Vector2.ZERO
+var BaseMaxSPEED = 800
+var BaseAcceleration = 500
+var BaseDeceleration = 500
 var cooldown = 0
 # what attack cooldown resets.
 var maxCooldown = 1
 
-@export var dash_strength = 100000000000000000
+@export var dash_strength = 2
 var dash_cooldown = 0
 var max_dash_cooldown = 10
-var dash_duration: float = .5
+var dash_duration: float = .25
 var dash_timer: float = 0.0
 var is_dashing: bool = false
-var dashing_direction: Vector2 = Vector2.ZERO
+var dashdirection
+
+var target = Vector2.ZERO
+var item_cooldown = 0
+var max_item_cooldown = 1
 
 #tileset variables
 @export var data_tile_map: TileMapLayer
@@ -32,35 +35,41 @@ func _physics_process(delta):
 		if tile_data != null: #if there is a data at that tile
 			#get the string thats in the tileCondition so Sticky ect
 			currentTitleData = tile_data.get_custom_data("TileCondition") 
-			print("TileCondition: ", currentTitleData)
+			#print("TileCondition: ", currentTitleData)
 		else:
-			print("No tile under player, setting to Crisp")
+			#print("No tile under player, setting to Crisp")
 			currentTitleData = "Crisp"
 	else:
 		push_error("TileMap reference is missing!")
 
-	var Acceleration = BaseAcceleration
-	var Deceleration = BaseDeceleration
+	var acceleration = BaseAcceleration
+	var deceleration = BaseDeceleration
+	var maxSPEED = BaseMaxSPEED
 
 #Balance speed values
 	var High = 2
 	var Low = 0.5
 	if currentTitleData == "Crisp":
-		Acceleration *= High
-		Deceleration *= High
+		acceleration *= High
+		deceleration *= High
 	if currentTitleData == "Slippery":
-		Acceleration *= Low
-		Deceleration *= Low
+		acceleration *= Low
+		deceleration *= Low
+		maxSPEED *= High
 	if currentTitleData == "Sticky" :
-		Acceleration *= Low
-		Deceleration *= High
+		acceleration *= Low
+		deceleration *= High
+		maxSPEED *= Low
+
+	# Make transitions 
+	cap_speed(maxSPEED)
 
 	var direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	
+	
 	if Input.is_action_just_pressed("dash") and not is_dashing and dash_cooldown == 0:
-		var dashdirection = (get_global_mouse_position()-global_position).normalized()
-		
-		velocity = dashdirection * dash_strength
+		dashdirection = (get_global_mouse_position()-global_position).normalized()
+		print(dashdirection)
 		dash_timer = dash_duration
 		is_dashing = true
 		print("dashing")
@@ -68,17 +77,16 @@ func _physics_process(delta):
 	# If dashing, reduce timer but still allow normal input smoothing
 	if is_dashing:
 		dash_timer -= delta
+		velocity = velocity.move_toward(dashdirection * maxSPEED, acceleration * dash_strength * delta)
 		print("slowing down dashing")
 		if dash_timer <= 0:
 			is_dashing = false
 			print("done dashing")
-
-	# Normal smooth movement only when not dashing
-	if not is_dashing:
-		if direction != Vector2.ZERO:
-			velocity = velocity.move_toward(direction * maxSPEED, Acceleration * delta)
-		else:
-			velocity = velocity.move_toward(Vector2.ZERO, Deceleration * delta)
+	
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction * maxSPEED, acceleration * delta)
+	elif not is_dashing || velocity.length() > maxSPEED:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 
 	move_and_slide()
 
@@ -88,18 +96,16 @@ func _physics_process(delta):
 		$AnimatedSprite2D.stop()
 #endregion
 
-	if item_cooldown <= 0:
+	if item_cooldown <= 0 && dash_cooldown <= 0:
 		look_at(get_global_mouse_position())
 	else:
-		item_cooldown -= delta
-	if dash_cooldown <= 0:
-		look_at(get_global_mouse_position())
-	else:
-		dash_cooldown -= delta
+		item_cooldown = max(item_cooldown - delta, 0)
+		dash_cooldown = max(dash_cooldown - delta, 0)
 
-var target = Vector2.ZERO
-var item_cooldown = 0
-var max_item_cooldown = 1
+# Call this function to cap the speed to the given speed
+func cap_speed(capped_speed) -> void:
+	if velocity.length() > capped_speed:
+		velocity = capped_speed * velocity.normalized()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
